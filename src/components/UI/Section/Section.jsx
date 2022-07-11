b20409de92468c6a6db50d5a4a96631918853d17
Olena-Milham <nikimilh@gmail.com>
2022-07-11 20:38:33 +0300

    update box

--- src/components/UI/Section/Section.jsx
+++ src/components/UI/Section/Section.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import { Box } from '../Box';
 import { Title } from './Section.styled';
 
 export const Section=({title, children})=>(
-    <>
+    <Box as='section' p={4}>
     <Title>{title}</Title>
     {children}
-    </>
+    </Box>
     
 )
 
